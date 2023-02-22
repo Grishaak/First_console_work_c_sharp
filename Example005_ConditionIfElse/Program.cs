630b2c4a9efa16fe166d1226acc81f25cb4b8702
Grigory Grebnev <antonianogroboracevskij@gmail.com>
2023-02-22 17:07:05 +0300

    add Example004-005 div and ifElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username == "Гриша")
+{
+    Console.WriteLine("Ух, ето кто? Гриша?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
